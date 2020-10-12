a6cd5c14a536f90fb0f8af9c53f3d1f202b45ede
alexander <alexander.shalamov@ukr.net>
2020-10-12 18:41:03 +0300

    Part-5.1 "Dependency Injection with Hilt"

--- app/src/main/java/com/example/imagesearch/MainActivity.kt
+++ app/src/main/java/com/example/imagesearch/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
 }
